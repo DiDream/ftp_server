6beec2c58aeeb66fd6c0cf2b4ea4d6a8da19cf32
DiDream <alexjimmy1993@hotmail.com>
2018-05-18 10:45:52 +0100

    define_socket_TCP. Identación. .gitignore añadido

--- ClientConnection.cpp
+++ ClientConnection.cpp
@@ -1,10 +1,10 @@
 //****************************************************************************
 //                         REDES Y SISTEMAS DISTRIBUIDOS
-//                      
+//
 //                     2ª de grado de Ingeniería Informática
-//                       
+//
 //              This class processes an FTP transactions.
-// 
+//
 //****************************************************************************
 
 
@@ -27,7 +27,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include <iostream>
 #include <dirent.h>
 
@@ -36,45 +36,37 @@
 #include "ClientConnection.h"
 
 
-
-
 ClientConnection::ClientConnection(int s) {
-    int sock = (int)(s);
-  
-    char buffer[MAX_BUFF];
+  int sock = (int)(s);
+  char buffer[MAX_BUFF];
 
-    control_socket = s;
-    // Check the Linux man pages to know what fdopen does.
-    fd = fdopen(s, "a+");
-    if (fd == NULL){
-	std::cout << "Connection closed" << std::endl;
+  control_socket = s;
+  // Check the Linux man pages to know what fdopen does.
+  fd = fdopen(s, "a+");
+  if (fd == NULL){
+	  std::cout << "Connection closed" << std::endl;
+  	fclose(fd);
+  	close(control_socket);
+  	ok = false;
+  	return ;
+  }
 
-	fclose(fd);
-	close(control_socket);
-	ok = false;
-	return ;
-    }
-    
-    ok = true;
-    data_socket = -1;
-   
-  
-  
+  ok = true;
+  data_socket = -1;
 };
 
 
 ClientConnection::~ClientConnection() {
  	fclose(fd);
-	close(control_socket); 
-  
+	close(control_socket);
+
 }
 
 
 int connect_TCP( uint32_t address,  uint16_t  port) {
-     // Implement your code to define a socket here
+  // Implement your code to define a socket here
 
-    return -1; // You must return the socket descriptor.
-
+  return -1; // You must return the socket descriptor.
 }
 
 
@@ -86,83 +78,75 @@ void ClientConnection::stop() {
     close(data_socket);
     close(control_socket);
     parar = true;
-  
 }
 
 
 
 
 
-    
+
 #define COMMAND(cmd) strcmp(command, cmd)==0
 
 // This method processes the requests.
 // Here you should implement the actions related to the FTP commands.
 // See the example for the USER command.
-// If you think that you have to add other commands feel free to do so. You 
+// If you think that you have to add other commands feel free to do so. You
 // are allowed to add auxiliary methods if necessary.
 
 void ClientConnection::WaitForRequests() {
-    if (!ok) {
-	 return;
+  if (!ok) return;
+
+  fprintf(fd, "220 Service ready\n");
+
+  while(!parar) {
+    fscanf(fd, "%s", command);
+
+    if (COMMAND("USER")) {
+      fscanf(fd, "%s", arg);
+      fprintf(fd, "331 User name ok, need password\n");
     }
-    
-    fprintf(fd, "220 Service ready\n");
-  
-    while(!parar) {
+    else if (COMMAND("PWD")) {
 
-
- 
-      fscanf(fd, "%s", command);
-      if (COMMAND("USER")) {
-	    fscanf(fd, "%s", arg);
-	    fprintf(fd, "331 User name ok, need password\n");
-      }
-      else if (COMMAND("PWD")) {
-	   
-      }
-      else if (COMMAND("PASS")) {
-	   
-      }
-      else if (COMMAND("PORT")) {
-	  
-      }
-      else if (COMMAND("PASV")) {
-	  
-      }
-      else if (COMMAND("CWD")) {
-	   
-      }
-      else if (COMMAND("STOR") ) {
-	    
-      }
-      else if (COMMAND("SYST")) {
-	   
-      }
-      else if (COMMAND("TYPE")) {
-	  
-      }
-      else if (COMMAND("RETR")) {
-	   
-      }
-      else if (COMMAND("QUIT")) {
-	 
-      }
-      else if (COMMAND("LIST")) {
-	
-      }
-      else  {
-	    fprintf(fd, "502 Command not implemented.\n"); fflush(fd);
-	    printf("Comando : %s %s\n", command, arg);
-	    printf("Error interno del servidor\n");
-	
-      }
-      
     }
-    
-    fclose(fd);
+    else if (COMMAND("PASS")) {
 
-    
-    return;
-  
+    }
+    else if (COMMAND("PORT")) {
+
+    }
+    else if (COMMAND("PASV")) {
+
+    }
+    else if (COMMAND("CWD")) {
+
+    }
+    else if (COMMAND("STOR") ) {
+
+    }
+    else if (COMMAND("SYST")) {
+
+    }
+    else if (COMMAND("TYPE")) {
+
+    }
+    else if (COMMAND("RETR")) {
+
+    }
+    else if (COMMAND("QUIT")) {
+
+    }
+    else if (COMMAND("LIST")) {
+
+    }
+    else {
+      fprintf(fd, "502 Command not implemented.\n"); fflush(fd);
+      printf("Comando : %s %s\n", command, arg);
+      printf("Error interno del servidor\n");
+    }
+  }
+
+  fclose(fd);
+
+  return;
+
 };
--- ftp_server.cpp
+++ ftp_server.cpp
@@ -6,27 +6,24 @@
 FTPServer *server;
 
 extern "C" void sighandler(int signal, siginfo_t *info, void *ptr) {
-  std::cout << "Dispara sigaction" << std::endl;  
+  std::cout << "Dispara sigaction" << std::endl;
   server->stop();
   exit(-1);
 }
 
 
 void exit_handler() {
-    server->stop();
+  server->stop();
 }
 
 
 int main(int argc, char **argv) {
+  struct sigaction action;
 
-  
-  
-    struct sigaction action;
-    action.sa_sigaction = sighandler;
-    action.sa_flags = SA_SIGINFO;
-      sigaction(SIGINT, &action , NULL);
-    server = new FTPServer(2121);
-      atexit(exit_handler);
-    server->run();
-
+  action.sa_sigaction = sighandler;
+  action.sa_flags = SA_SIGINFO;
+  sigaction(SIGINT, &action , NULL);
+  server = new FTPServer(2121);
+  atexit(exit_handler);
+  server->run();
 }
